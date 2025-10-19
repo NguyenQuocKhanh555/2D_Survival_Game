b45f213156f828bbb0ed8edcf3c5c36e38011b18
NguyenQuocKhanh <raycrop555@gmail.com>
2025-10-19 13:15:16 +0700

    Control the player
    Action: Move (Walk, Run)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 1.0f;
+    [SerializeField] private float runSpeed = 1.0f;
+
+    private PlayerControls playerControls;
+    private Rigidbody2D rb;
+
+    private Vector2 moveInput;
+    private bool isRunning;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        moveInput = playerControls.PlayerMovement.Move.ReadValue<Vector2>();
+        bool isShiftPressed = Keyboard.current.leftShiftKey.isPressed || Keyboard.current.rightShiftKey.isPressed;
+        isRunning = isShiftPressed;
+    }
+
+    private void Move()
+    {
+        float currentSpeed = isRunning ? runSpeed : walkSpeed;
+        Vector2 movement = moveInput * currentSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movement);
+    }
+}
