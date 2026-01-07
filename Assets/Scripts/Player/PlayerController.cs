76975e7a854f76ffc3f501f988486179729dee78
BrianOtaku <brainotaku11@gmail.com>
2026-01-07 21:01:04 +0700

    Set up

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -129,8 +129,12 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         if (_isDodging) return;
-        Vector2 movement = _moveInput * _walkSpeed * Time.fixedDeltaTime;
-        _rb.MovePosition(_rb.position + movement);
+        _rb.linearVelocity = _moveInput * _walkSpeed;
+        //Vector3 movement = _moveInput * _walkSpeed * Time.fixedDeltaTime;
+        //_rb.MovePosition(transform.position + movement);
+        Vector3 pos = transform.position;
+        pos.z = pos.y * 0.0001f;
+        transform.position = pos;
     }
 
     private void OnDodge(InputAction.CallbackContext context)
---
+++ Assets/Scripts/ZDepth.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ZDepth : MonoBehaviour
+{
+    Transform t;
+    [SerializeField] bool stationary = true;
+
+    private void Start()
+    {
+        t = transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+        pos.z = pos.y * 0.0001f;
+        transform.position = pos;
+
+        if (stationary) { Destroy(this); }
+    }
+}
